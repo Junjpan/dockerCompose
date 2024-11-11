cfe2ac8fc240b1b36a56874675a901760901ed72
JunjPan <elisepjj@hotmail.com>
2024-11-10 16:53:03 -0800

    use docker compose to run multiple service

---
+++ api/app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.json([
+    {
+      id: "001",
+      title: "Book Review: The Bear & The Nightingale.",
+    },
+    {
+      id: "002",
+      title: "Game Review: Pokemon Brillian Diamond.",
+    },
+    {
+      id: "003",
+      title: "Show Review: Alice in Borderland",
+    },
+    {
+      id: "004",
+      title: "Show Review: Alice in Borderland",
+    },
+  ]);
+});
+
+app.listen(4000, () => {
+  console.log("listening for requests on port 4000");
+});
